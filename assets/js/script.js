83e6dd6ae09f8e456dbf089186b76338e01e22a4
Rana Saifullah <111068945+Rana-Saif-ullah@users.noreply.github.com>
2023-09-12 19:14:13 +0500

    making the other things

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,23 @@ menu.addEventListener("click", function () {
     menu.classList.toggle("active__menu");
     nav__links.classList.toggle("active__navlinks");
 });
+
+
+
+let mixerPortfolio = mixitup('.section__cards', {
+    selectors: {
+        target: '.card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+const linkWork = document.querySelectorAll('.tab__link')
+
+function activework(){
+    linkWork.forEach(l=> l.classList.remove('sub__title'))
+    this.classList.add('sub__title')
+}
+
+linkWork.forEach(l=> l.addEventListener('click', activework))
