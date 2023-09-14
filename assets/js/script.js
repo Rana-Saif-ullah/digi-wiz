8ab7b815b26ac1b4c02c35d1a3f8a44ca54fe129
Rana Saifullah <111068945+Rana-Saif-ullah@users.noreply.github.com>
2023-09-14 12:37:03 +0500

    making contact from better

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,21 +8,78 @@ menu.addEventListener("click", function () {
 
 
 
-let mixerPortfolio = mixitup('.section__cards', {
-    selectors: {
-        target: '.card'
+// let mixerPortfolio = mixitup('.section__cards', {
+//     selectors: {
+//         target: '.card'
+//     },
+//     animation: {
+//         duration: 300
+//     }
+// });
+
+// const linkWork = document.querySelectorAll('.tab__link')
+
+// function activework() {
+//     linkWork.forEach(l => l.classList.remove('sub__title'))
+//     this.classList.add('sub__title')
+// }
+
+// linkWork.forEach(l => l.addEventListener('click', activework))
+
+
+
+const arr = [
+    {
+        text: "Rizwzn",
+        role: "CEO",
     },
-    animation: {
-        duration: 300
+    {
+        text: "Saif",
+        role: "CEO",
+    },
+    {
+        text: "Zeeshan",
+        role: "CEO",
+    },
+    {
+        text: "Rizwzn",
+        role: "CEO",
+    },
+    {
+        text: "Rizwzn",
+        role: "CEO",
+    },
+    {
+        text: "Rizasdfasdfsdfsdrfwzn",
+        role: "CEO",
     }
+]
+let first = 0;
+let last = 1;
+const arraySlice = arr.slice(first, last)
+
+arraySlice.forEach(item => {
+    document.getElementById("testimonial__slider").innerHTML +=
+        `
+        <div class="slide">
+            <img src="assets/images/“.png" .png alt="">
+            <p class="paragraph">
+               We have the best creative team that bring your ideas to life
+             </p>
+            <img src="assets/images/profile.png" alt="Profile">
+            <h1 class="small__heading">
+                ${item.text}
+            </h1>
+            <p class="paragraph">
+                ${item.role}
+            </p>
+        </div>
+    `
 });
 
-const linkWork = document.querySelectorAll('.tab__link')
 
-function activework() {
-    linkWork.forEach(l => l.classList.remove('sub__title'))
-    this.classList.add('sub__title')
+function slider(){
+    var left = document.getElementById(".left")
 }
 
-linkWork.forEach(l => l.addEventListener('click', activework))
-
+console.log(slider())
